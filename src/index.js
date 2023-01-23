ebfe18e2ec0b5bb9ec576ae83c4d9b5fec6248e7
LenumaYu <elena_guseva_92@mail.ru>
2023-01-23 12:07:13 +0700

    finish task witn reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        return Number(String(n).split('').reverse().join(''));
+    }
+    return Number(String(n*(-1)).split('').reverse().join(''));
 }
